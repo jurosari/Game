1286f7d2e5e808ff6aeed3ce4953e72f5a5aefeb
Juan Rosario <onlyjuanwork@gmail.com>
2025-07-10 21:51:53 -0400

    Added more roles for each class, alongside with inheritance

--- game.cpp
+++ game.cpp
@@ -9,12 +9,43 @@ I want to build a video game where you are controlling 2 character in a turn bas
 #include <iostream>
 
 class Character{
+    public:
+        int Name;
+        int HP;
+        int ATK;
+        int DEF;
+        //int Stats[2] = [HP,ATK,DEF]; //I want to store the selected variables here, but there's an error
+        //std::string Category[2] = ["Warrior", "Mage"]; //I want the player to select one of the 2 or both of the category
+        std::string MoveSet[];
+        
+
+        
+        void Introduction(){
+                    
+        }
+
+        void MoveUse(){
+
+        }
+
+        void Faint(){
+
+        }
 };
 
 class Player{
-
+    public:
+        std::string MoveEffect[4];
 };
 
 class Enemy{
-
+    public:
+        std::string MoveEffect[3];
 };
+
+
+int main(){
+    
+
+    return 0;
+}
