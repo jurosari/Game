0de827082e02ac50694242a4c02c5cd0b4ecd2a8
Juan Rosario <onlyjuanwork@gmail.com>
2025-07-10 21:18:28 -0400

    First commit

---
+++ .history/game_20250524193349.cpp
@@ -0,0 +1,7 @@
+/*
+I want to build a video game where you are controlling 2 character in a turn based game.
+- The only 2 thing that the user can customize is the type of warrior and name you'll be using
+- Each warrior have 3 moves (1 attack, 1 buff (ally) or debuff (enemy) and 1 fusion)
+- Fusion combines all stats (Health Points, attack, defense and speed), moves (instead of 2 moves, it will be 4) and their names! (First Half and Second Half)
+
+*/ 
